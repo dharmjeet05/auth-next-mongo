1173b0f3f35a8ce191897822649b773c7b8a2ecb
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2025-07-02 10:49:56 +0530

    Working on signup api

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { connect } from "@/db/dbConfig";
+import User from "@/models/user.model";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = request.json();
+    const { username, email, password }: any = reqBody;
+
+    // validation
+
+    // check if user already exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exist" },
+        { status: 400 }
+      );
+    }
+
+    // encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // save user to the db
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
